78a866fbba4332a21c55a604568d5eaa6d4418b0
Alex Dunn <alexander.dunn@outlook.com>
2020-07-14 13:48:17 -0400

    added content group

---
+++ src/Voicify.Sdk.Core/Voicify.Sdk.Core.Models/Content/ContentGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Voicify.Sdk.Core.Models.Content
+{
+    public class ContentGroup
+    {
+        public string[] Content { get; set; }
+        public string[] FollowUps { get; set; }
+    }
+}
